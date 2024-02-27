032f9b5d7cc8f301aae0686d4138be3785d2f6a8
Camille Mallet <mallet.camille11@gmail.com>
2024-02-27 14:53:28 +0100

    feat : add Error page

--- front-end/src/pages/Error/Error.jsx
+++ front-end/src/pages/Error/Error.jsx
@@ -1,8 +1,13 @@
 import '../../index.css'
+import {Link} from 'react-router-dom'
 
 function Error () {
 return (
-    <span>Ceci est la page d'erreur</span>
+    <div className='error__container'>
+        <span className='error__404'>Error 404</span>
+        <span className='error__txt'>Oups ! La page que vous demandez n'existe pas.</span>
+        <Link className='error__link' to="/">Retourner sur la page d'accueil</Link> 
+    </div>
 )
 }
 
